6cada6d0fc0cab3237442916dd705991b765e16d
denisftw <denny2006@gmail.com>
2019-07-20 19:00:58 +1000

    A better logger name and removing an unused logger

--- src/main/g8/src/main/scala/com/appliedscala/quickstart/Main.scala
+++ src/main/g8/src/main/scala/com/appliedscala/quickstart/Main.scala
@@ -3,7 +3,7 @@ package com.appliedscala.quickstart
 import com.typesafe.scalalogging.Logger
 
 object Main {
-  private val log = Logger(this.getClass)
+  private val log = Logger("Main")
 
   def main(args: Array[String]): Unit = {
     log.info("Starting the application")
